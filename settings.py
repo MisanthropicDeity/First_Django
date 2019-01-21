df2449507cb9156a3864d4269d844cb447e5169d
Vidhu Shikhar Joshi <vidhujoshi31@gmail.com>
2019-01-21 18:50:14 +0530

    mistake corrected

--- settings.py
+++ settings.py
@@ -127,5 +127,5 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'staitc')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
